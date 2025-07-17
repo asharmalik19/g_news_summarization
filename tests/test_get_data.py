6bdd3ca13c58b2709685a0775b427ac54e153a57
Ashar <asharmalik.khan@gmail.com>
2025-07-17 15:24:55 +0500

    Add test for clean_the_data function

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -3,15 +3,20 @@ import os
 import asyncio
 
 import pytest
+import pandas as pd
 
-from data_extraction_pipeline.get_data import get_articles
+# Add the project root to Python path
+sys.path.insert(0,
+os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-async def test_get_articles():
-    links = [
-        'https://www.ft.com/content/ad5a6df0-da1d-4246-80c2-387d5cc7c541'
-    ]
-    articles = await get_articles(links)
-    article = articles[0]
-    # assert article['t
-    print(article)
+from data_extraction_pipeline.get_data import clean_the_data
+
+def test_clean_the_data():
+    df = pd.DataFrame({
+        'text': ['Good article', 'Try unlimited access to read more', ''],
+        'title': ['Title 1', 'Title 2', 'Title 3']
+    })
+    cleaned_df = clean_the_data(df)
+    assert cleaned_df.shape[0] == 1
+    assert cleaned_df.iloc[0]['text'] == 'Good article'
 
